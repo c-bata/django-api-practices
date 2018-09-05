10d57e04827de030bf6df0c93d6b2b71260d991f
c-bata <contact@c-bata.link>
2018-09-06 03:13:41 +0900

    Add custom username validator

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin, UserManager
-from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+from .validators import UsernameValidator
+
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username_validator = ASCIIUsernameValidator()
+    username_validator = UsernameValidator()
     username = models.CharField(
         _('username'),
         max_length=150,
