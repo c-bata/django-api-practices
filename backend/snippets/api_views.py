8a09e90acb612a63099790509ddd9ce403849b04
c-bata <contact@c-bata.link>
2018-09-17 13:05:34 +0900

    Remove pagination_class

--- backend/snippets/api_views.py
+++ backend/snippets/api_views.py
@@ -1,19 +1,13 @@
 from rest_framework import viewsets, filters
-from rest_framework.pagination import CursorPagination
 
 from .models import Snippet, Comment
 from .serializers import SnippetSerializer, CommentSerializer
-
-
-class SnippetPagination(CursorPagination):
-    ordering = '-created_at'
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     filter_backends = (filters.OrderingFilter,)
-    pagination_class = SnippetPagination
     ordering_fields = ('id', 'created_at',)
     ordering = ('created_at',)
 
